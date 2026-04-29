03d6f2371bbd4369d475f1f32ee3e8e90c04ba38
lulu <lulu@LAPTOP-LuLu.localdomain>
2026-04-29 12:54:50 +0200

    UI saturate header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 export default function Header() {
   return (
     <header className={`${styles.header_layout}  bg-transparent lg:w-1/4 lg:max-w-64 xl:ml-20 ` }>
-      <img className={`${styles.header_photo}  shadow-2xl shadow-zinc-800/50  lg:mt-12`} src="images/photo.png" alt="photo" />
+      <img className={`${styles.header_photo}  saturate-50 shadow-2xl shadow-zinc-800/50  lg:mt-12`} src="images/photo.png" alt="photo" />
         <Navbar/>
     </header>
   )
